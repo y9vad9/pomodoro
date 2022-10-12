e7a80dd213a72caff510303501957de3f153c8e9
y9vad9 <y9vad9@gmail.com>
2022-10-13 01:07:21 +0300

    feat: added check for access in `GetTimerUseCase`

--- backend/use-cases/src/main/kotlin/com/y9vad9/pomodoro/backend/usecases/timers/GetTimerUseCase.kt
+++ backend/use-cases/src/main/kotlin/com/y9vad9/pomodoro/backend/usecases/timers/GetTimerUseCase.kt
@@ -1,12 +1,18 @@
 package com.y9vad9.pomodoro.backend.usecases.timers
 
 import com.y9vad9.pomodoro.backend.repositories.TimersRepository
+import com.y9vad9.pomodoro.backend.repositories.UsersRepository
 
 class GetTimerUseCase(
     private val timers: TimersRepository
 ) {
-    suspend operator fun invoke(timerId: TimersRepository.TimerId): Result {
-        return Result.Success(timers.getTimer(timerId) ?: return Result.NotFound)
+    suspend operator fun invoke(
+        userId: UsersRepository.UserId,
+        timerId: TimersRepository.TimerId
+    ): Result {
+        return if (timers.isMemberOf(userId, timerId))
+            Result.Success(timers.getTimer(timerId) ?: return Result.NotFound)
+        else Result.NotFound
     }
 
     sealed interface Result {
