0015ef5e33d514314a26da9ad7a9913b5f2278bb
y9vad9 <y9vad9@gmail.com>
2022-10-13 01:49:54 +0300

    feat: added google-auth integration

---
+++ backend/application/src/main/kotlin/com/y9vad9/pomodoro/backend/application/routes/auth/google.kt
@@ -0,0 +1,35 @@
+package com.y9vad9.pomodoro.backend.application.routes.auth
+
+import com.y9vad9.pomodoro.backend.usecases.auth.AuthViaGoogleUseCase
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+class AuthViaGoogleRequest(
+    val code: String
+) {
+    @Serializable
+    sealed interface Result {
+        @JvmInline
+        value class Success(val accessToken: String) : Result
+    }
+}
+
+fun Route.authViaGoogle(authViaGoogle: AuthViaGoogleUseCase) {
+    post("google") {
+        val data: AuthViaGoogleRequest = call.receive()
+        val response = when (val result = authViaGoogle(data.code)) {
+            is AuthViaGoogleUseCase.Result.InvalidAuthorization -> {
+                call.respond(HttpStatusCode.Unauthorized)
+                return@post
+            }
+
+            is AuthViaGoogleUseCase.Result.Success ->
+                AuthViaGoogleRequest.Result.Success(result.accessToken.string)
+        }
+    }
+}
---
+++ backend/use-cases/src/main/kotlin/com/y9vad9/pomodoro/backend/usecases/auth/AuthViaGoogleUseCase.kt
@@ -0,0 +1,48 @@
+package com.y9vad9.pomodoro.backend.usecases.auth
+
+import com.y9vad9.pomodoro.backend.domain.UserName
+import com.y9vad9.pomodoro.backend.google.auth.GoogleClient
+import com.y9vad9.pomodoro.backend.provider.AccessTokenProvider
+import com.y9vad9.pomodoro.backend.provider.CurrentTimeProvider
+import com.y9vad9.pomodoro.backend.repositories.AuthorizationsRepository
+import com.y9vad9.pomodoro.backend.repositories.LinkedSocialsRepository
+import com.y9vad9.pomodoro.backend.repositories.UsersRepository
+
+class AuthViaGoogleUseCase(
+    private val linkedSocials: LinkedSocialsRepository,
+    private val users: UsersRepository,
+    private val tokensProvider: AccessTokenProvider,
+    private val authorizations: AuthorizationsRepository,
+    private val time: CurrentTimeProvider,
+    private val googleClient: GoogleClient
+) {
+    suspend operator fun invoke(code: String): Result {
+        val accessTokenResult = googleClient.getAccessToken(code, "about:blank")
+            ?: return Result.InvalidAuthorization
+        val user = googleClient.getUserProfile(accessTokenResult)
+        val linked = linkedSocials.getBySocial(
+            LinkedSocialsRepository.Social.Google(
+                accessTokenResult.id,
+                user.email
+            )
+        )
+
+        return if (linked == null) {
+            val id = users.createUser(UserName(user.name), time.provide())
+            val accessToken = tokensProvider.provide()
+            authorizations.create(id, accessToken, time.provide())
+            Result.Success(accessToken)
+        } else {
+            val accessToken = tokensProvider.provide()
+            authorizations.create(linked, tokensProvider.provide(), time.provide())
+            Result.Success(accessToken)
+        }
+    }
+
+    sealed interface Result {
+        object InvalidAuthorization : Result
+
+        @JvmInline
+        value class Success(val accessToken: AuthorizationsRepository.AccessToken) : Result
+    }
+}
