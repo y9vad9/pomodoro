186721ceca89431b35cb4ade7470926db3b8a283
Vadim Yaroschuk <y9vad9@gmail.com>
2022-10-28 21:37:26 +0300

    fix: table column name (#12)

--- backend/adapters/repositories-integration/src/main/kotlin/com/y9vad9/pomodoro/backend/repositories/integration/tables/TimerParticipantsTable.kt
+++ backend/adapters/repositories-integration/src/main/kotlin/com/y9vad9/pomodoro/backend/repositories/integration/tables/TimerParticipantsTable.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.select
 object TimerParticipantsTable : Table() {
     val TIMER_ID = integer("timer_id")
         .references(TimersTable.TIMER_ID, onDelete = ReferenceOption.CASCADE)
-    val PARTICIPANT_ID = integer("")
+    val PARTICIPANT_ID = integer("participants_id")
         .references(UsersTable.USER_ID, onDelete = ReferenceOption.CASCADE)
 
     private val uniqueKey = uniqueIndex(TIMER_ID, PARTICIPANT_ID)
