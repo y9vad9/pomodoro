a5185f7835eefc0b3b96cb4a022526aaf1ab5c33
Vadim Yaroschuk <y9vad9@gmail.com>
2022-11-29 02:05:19 +0200

    fix: conflicts

--- backend/application/src/main/kotlin/com/y9vad9/pomodoro/backend/application/types/value/TimerEventId.kt
+++ backend/application/src/main/kotlin/com/y9vad9/pomodoro/backend/application/types/value/TimerEventId.kt
@@ -1,10 +1,7 @@
 package com.y9vad9.pomodoro.backend.application.types.value
 
-import com.y9vad9.pomodoro.backend.repositories.TimersRepository
 import kotlinx.serialization.Serializable
 
 @Serializable
 @JvmInline
 value class TimerEventId(val long: Long)
-
-fun TimersRepository.TimerEvent.TimerEventId.serializable() = TimerEventId(long)
